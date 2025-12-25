6aa83c58b4868b93387d1be9ce4d1ac22de80f5d
Zavion Cyboron <z.cyboron07@gmail.com>
2025-12-25 09:21:31 -0700

    feat: add Mecanum motor configs

---
+++ src/main/kotlin/org/hangar84/robot2026/mecanum/MecanumConfigs.kt
@@ -0,0 +1,29 @@
+package org.hangar84.robot2026.mecanum
+
+import com.revrobotics.spark.config.ClosedLoopConfig
+import com.revrobotics.spark.config.SparkBaseConfig
+import com.revrobotics.spark.config.SparkMaxConfig
+
+object MecanumConfigs {
+    private const val WHEEL_DIAMETER_M = 0.14732
+    private const val WHEEL_CIRCUMFERENCE_M = WHEEL_DIAMETER_M * Math.PI
+    private const val GEAR_RATIO = 4.5
+
+    private const val  POSITION_FACTOR_M = WHEEL_CIRCUMFERENCE_M / GEAR_RATIO
+    private const val VELOCITY_FACTOR_MPS = POSITION_FACTOR_M / 60.0
+
+    val driveConfig: SparkMaxConfig = SparkMaxConfig()
+
+    init {
+        driveConfig
+            .idleMode(SparkBaseConfig.IdleMode.kBrake)
+            .smartCurrentLimit(40)
+        driveConfig.encoder
+            .positionConversionFactor(POSITION_FACTOR_M)
+            .velocityConversionFactor(VELOCITY_FACTOR_MPS)
+        driveConfig.closedLoop
+            .feedbackSensor(ClosedLoopConfig.FeedbackSensor.kPrimaryEncoder)
+            .pidf(0.0002, 0.0, 0.0, 0.0)
+            .outputRange(-1.0, 1.0)
+    }
+}
