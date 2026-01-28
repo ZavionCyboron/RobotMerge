05d49d3298e5ed39a4101087729050a20be0aa6f
Conner Nolan <connernolan74@gmail.com>
2026-01-27 19:59:33 -0700

    feat: Adding Chassis speed as Drivetrain requirment

--- src/main/kotlin/org/hangar84/robot2026/subsystems/DriveTrain.kt
+++ src/main/kotlin/org/hangar84/robot2026/subsystems/DriveTrain.kt
@@ -2,6 +2,7 @@ package org.hangar84.robot2026.subsystems
 
 import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.math.geometry.Rotation2d
+import edu.wpi.first.math.kinematics.ChassisSpeeds
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
@@ -13,6 +14,7 @@ abstract class Drivetrain : SubsystemBase() {
 
     abstract fun drive(xSpeed: Double, ySpeed: Double, rot: Double, fieldRelative: Boolean)
     abstract fun buildAutoChooser(): SendableChooser<Command>
+    abstract fun getChassisSpeeds(): ChassisSpeeds
 
     abstract fun getHeading(): Rotation2d
     abstract fun resetPose(pose: Pose2d)
