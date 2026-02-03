410754b65b3d12476054b1fb9ca20ba3656e50ba
Conner Nolan <connernolan74@gmail.com>
2026-02-02 22:54:34 -0700

    Fix: Changing from leftvelocityRpm - LeftTempCelcius in IO

--- src/main/kotlin/org/hangar84/robot2026/io/IntakeIO.kt
+++ src/main/kotlin/org/hangar84/robot2026/io/IntakeIO.kt
@@ -3,12 +3,11 @@ package org.hangar84.robot2026.io
 interface IntakeIO {
     data class Inputs(
         var leftAppliedOutput: Double = 0.0,
-        var leftVelocityRpm: Double = 0.0,
+        var leftTempCelcius: Double = 0.0,
         var leftCurrentAmps: Double = 0.0,
     )
 
     fun updateInputs(inputs: Inputs) {}
     fun setPercent(percent: Double) {}
     fun stop() = setPercent(0.0)
-    fun simulationPeriodic(dtSeconds: Double) {}
 }
